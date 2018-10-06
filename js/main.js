c6e82c6909b406721373a77295b05a37e548f2ce
fitsum belayneh <fitsu_alt@yahoo.com>
2018-10-06 11:45:08 -0400

    validating the form

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ function saveBookmarks(e) {
   var siteName = document.getElementById("siteName").value;
   var siteUrl = document.getElementById("siteUrl").value;
 
+  if (!validateForm(siteName, siteUrl)) {
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -90,4 +94,21 @@ function fetchBookmarks() {
       "</h3>" +
       "</div>";
   }
+}
+
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert("please fill the form");
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert("Please use a valid url");
+    return false;
+  }
+
+  return true;
 }
